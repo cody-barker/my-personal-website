4a39b648308a496725c238f1c2c60f11ce870a22
Cody Barker <codybarker.or@gmail.com>
2022-08-31 16:40:41 -0700

    save

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const h2 = document.createElement("h2");
-h2.textContent = "From Hemp to Programming";
+/*  This document needs to have at least some Javascript.
 
-document.querySelector("body").appendChild(h2);
+const h2 = document.createElement("h2");
+h2.textContent = "How to Grow Hemo";
+
+document.querySelector("body").appendChild(h2); */
