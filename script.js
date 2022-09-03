04c318c14d388571ae602a62c9933718fa37bc68
Cody Barker <codybarker.or@gmail.com>
2022-09-03 15:51:39 -0700

    trying to add loop video to home

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 /*  This document needs to have at least some Javascript.
 
 const h2 = document.createElement("h2");
-h2.textContent = "How to Grow Hemo";
+h2.textContent = "How to Grow Hemp";
 
 document.querySelector("body").appendChild(h2); */
 
 
-const landingText = .landingText
